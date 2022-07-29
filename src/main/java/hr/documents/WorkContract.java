ffdb451d8abfb3a8e7d433c582b975f75e3b52f8
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-07-29 12:17:01 +0400

    implements all functional interfaces

---
+++ src/main/java/hr/documents/WorkContract.java
@@ -0,0 +1,28 @@
+package hr.documents;
+
+public class WorkContract implements
+ExportableText,
+ExportableJson,
+ExportablePdf {
+
+    private String content;
+
+    public WorkContract(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toJson() {
+        return "{'content':'" + this.content + "'}";
+    }
+
+    @Override
+    public byte[] toPdf() {
+        return content.getBytes();
+    }
+
+    @Override
+    public String toTxt() {
+        return this.content;
+    }
+}
